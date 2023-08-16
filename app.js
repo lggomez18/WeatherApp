febddd685779b27fe6fb04ed5c2fd34421500e80
Leticia Gmx <137099040+lggomez18@users.noreply.github.com>
2023-08-16 00:25:19 -0500

    Fixed unsuccessfully unit converter

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
+
 //Button Geolocation API
 
 function showPosition(position) {
   let apiKey = "57d09144bf433da24574a6e95f14182c";
-  let units = "metric";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=${units}`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showTemperature);
 }
 
@@ -21,65 +21,47 @@ function search(event) {
   searchCity(city.value);
 }
 
+
+
 function searchCity(city) {
-  let units = "metric";
   let apiKey = "57d09144bf433da24574a6e95f14182c";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(showTemperature);
 }
 
 
-//Temperature
-function displayFahrenheitTemperature(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temp");
-  let fahrenheitTemperature= (celsiusTemperature * 9) / 5 + 32;
-  temperatureElement.innerHTML= Math.round(fahrenheitTemperature);
-}
 
-function displayCelsiusTemperature(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temp");
-  temperatureElement.innerHTML= Math.round(celsiusTemperature);
-}
+
+const temperatureElement = document.querySelector("#temp");
+const form = document.querySelector("#searchForm");
+const fahrenheitLink = document.querySelector("#fahrenheit-link");
+const celsiusLink = document.querySelector("#celsius-link");
 
 let celsiusTemperature = null;
 
 
-let form = document.querySelector("#searchForm");
+function displayFahrenheitTemperature() {
+  const fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function displayCelsiusTemperature() {
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
 form.addEventListener("submit", search);
 
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
-
-let celsuisLink = document.querySelector("#celsius-link");
-celsuisLink.addEventListener("click", displayCelsiusTemperature);
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 
-
-
-
-
-
-//Current Format Time & Date
-function formatDate(timestamp) {
-  let date = new Date(timestamp);
-  let hours = date.getHours();
-  let minutes = date.getMinutes();
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
-  let day = days[date.getDay()];
-  return `Last updated on: ${day} ${hours}:${minutes}`;
-}
 
 //ShowTemperaturesforCities
 
 function showTemperature(response) {
   let temperatureElement = document.querySelector("#temp");
   let cityElement = document.querySelector("#city");
-  //let descriptionElement = document.querySelector ("#description")
-  //let humidityElement = document.querySelector("humidity");
-  //let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
   let iconElement=document.querySelector("#icon");
   
@@ -95,3 +77,15 @@ function showTemperature(response) {
 
 
 }
+
+
+//Current Format Time & Date
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+  let day = days[date.getDay()];
+  return `Last updated on: ${day} ${hours}:${minutes}`;
+}
+
